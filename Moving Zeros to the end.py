bbca402cafe07f2a55944741ab3ff646f738864d
joshred83 <joshuapfarina@gmail.com>
2018-07-22 12:24:02 -0400

    added rudimentary tests

--- Moving Zeros to the end.py
+++ Moving Zeros to the end.py
@@ -6,7 +6,6 @@ move_zeros([false,1,0,1,2,0,1,3,"a"]) # returns[false,1,1,2,1,3,"a",0,0]"""
 
 def move_zeros(array):
     num_zeros = array.count(0)
-    print(num_zeros)
     new_list = []
 
     for x in array:
@@ -16,5 +15,23 @@ def move_zeros(array):
         new_list.append(0)
     return new_list
 
-print(move_zeros(["a",0,0,"b",None,"c","d",0,1,False,0,1,0,3,[],0,1,9,0,0,{},0,0,9]))
-print(["a","b",None,"c","d",1,False,1,3,[],1,9,{},9,0,0,0,0,0,0,0,0,0,0])
+print()
+print("===Basic tests===")
+print()
+print ('1 ', move_zeros([1, 2, 0, 1, 0, 1, 0, 3, 0, 1]) == [1, 2, 1, 1, 3, 1, 0, 0, 0, 0])
+
+print ('2 ', move_zeros([9, 0.0, 0, 9, 1, 2, 0, 1, 0, 1, 0.0, 3, 0, 1, 9, 0, 0, 0, 0, 9]) ==
+        [9, 9, 1, 2, 1, 1, 3, 1, 9, 9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+
+print ('3 ', move_zeros(["a", 0, 0, "b", "c", "d", 0, 1, 0, 1, 0, 3, 0, 1, 9, 0, 0, 0, 0, 9]) ==
+        ["a", "b", "c", "d", 1, 1, 3, 1, 9, 9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+
+print ('4 ',move_zeros(["a", 0, 0, "b", None, "c", "d", 0, 1, False, 0, 1, 0, 3, [], 0, 1, 9, 0, 0, {}, 0, 0, 9]) ==
+        ["a", "b", None, "c", "d", 1, False, 1, 3, [], 1, 9, {}, 9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+
+print ('5 ',move_zeros([0, 1, None, 2, False, 1, 0]) == [1, None, 2, False, 1, 0, 0])
+print ('6 ', move_zeros(["a", "b"]) == ["a", "b"])
+print ('7 ', move_zeros(["a"]) == ["a"])
+print ('8 ', move_zeros([0, 0]) == [0, 0])
+print ('9 ', move_zeros([0]) == [0])
+print ('10', move_zeros([]) == [])
